0c4d2f775bfd22b09772d409b4d8088084a5c6f3
raebaexxx <vadimfeda@yandex.ru>
2025-10-06 01:21:22 +0300

    Revert "Revert switch to OSS Audio"
    This reverts commit 08a2a6aa1fa87b8defcd1cad124dc2902a892f4c.

--- device.mk
+++ device.mk
@@ -13,6 +13,11 @@ $(call inherit-product, vendor/xiaomi/fuxi/fuxi-vendor.mk)
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
+    $(LOCAL_PATH)/configs/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
+
 # eUICC
 PRODUCT_PACKAGES += \
     XiaomiEuicc
