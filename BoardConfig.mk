95d6e39d9091a07c65d9391f33d6ec08dd3f6201
raebaexxx <vadimfeda@yandex.ru>
2025-10-10 01:14:06 +0300

    2 version for include miui cam

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,7 @@ DEVICE_PATH := device/xiaomi/fuxi
 
 # Camera
 TARGET_USES_MIUI_CAMERA := true
+# TARGET_INCLUDES_MIUI_CAMERA := true
 
 # Inherit from sm8550-common
 include device/xiaomi/sm8550-common/BoardConfigCommon.mk
