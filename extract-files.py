432552614bed8b72751c3b0f40db8e9daa78a62f
raebaexxx <vadimfeda@yandex.ru>
2025-10-09 20:31:20 +0300

    Patch citsensorservice to work with OSS libdisplayconfig.qti For some reason xiaomi has duplicated the Init method as ClientImplGet. (or it gets automatically generated for them due to some other differences?) However, our OSS libdisplayconfig.qti doesn't provide this symbol, but the Init method takes the same parameters and seems to be sufficient for citsensorservice to work.

--- extract-files.py
+++ extract-files.py
@@ -33,6 +33,8 @@ lib_fixups: lib_fixups_user_type = {
     libs_proto_3_9_1: lib_fixup_vendorcompat,
 }
 
+sensor_simbol = b'_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
 blob_fixups: blob_fixups_user_type = {
     (
         'odm/etc/camera/enhance_motiontuning.xml',
@@ -70,6 +72,10 @@ blob_fixups: blob_fixups_user_type = {
         'odm/lib64/libxmi_high_dynamic_range_cdsp.so',
     ): blob_fixup()
         .strip_debug_sections(),
+    (
+        'odm/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so',
+    ): blob_fixup()
+        .binary_regex_replace(b'_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE', sensor_simbol)
 }
 
 module = ExtractUtilsModule(
