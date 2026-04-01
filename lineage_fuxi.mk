ca1e1c8a37d15249b9503f4a8d6d1aa07b5c320f
raebaexxx <vadimfeda@yandex.ru>
2026-04-01 17:39:50 +0300

    Revert "update esim for cn device"
    This reverts commit e719862078b5988de2a2bcd5c5daf5598e86f9b2.

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -5,8 +5,8 @@
 #
 
 # Inherit common AOSP configurations
-$(call inherit-product, build/make/target/product/full_base_telephony.mk)
-$(call inherit-product, build/make/target/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device-specific configurations
 $(call inherit-product, device/xiaomi/fuxi/device.mk)
