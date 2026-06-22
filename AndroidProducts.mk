3bada9e5b87d40348b77cc28a7007860ced61f70
raebaexxx <vadimfeda@yandex.ru>
2026-06-22 03:03:49 +0300

    adapt to inf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/custom_fuxi.mk
+    $(LOCAL_DIR)/infinity_fuxi.mk
 
 COMMON_LUNCH_CHOICES := \
-    custom_fuxi-eng \
-    custom_fuxi-user \
-    custom_fuxi-userdebug
+    infinity_fuxi-eng \
+    infinity_fuxi-user \
+    infinity_fuxi-userdebug
--- infinity_fuxi.mk
+++ infinity_fuxi.mk
@@ -19,12 +19,19 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Rom flags
 TARGET_DISABLE_EPPE := true
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_ENABLE_BLUR := true
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_HAS_UDFPS := true
+INFINITY_MAINTAINER := "RaeBaeXXX"
+WITH_GAPPS := true
+
 
 # Device identifier
 PRODUCT_DEVICE := fuxi
-PRODUCT_NAME := custom_fuxi
+PRODUCT_NAME := infinity_fuxi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := 2211133C
+PRODUCT_MODEL := Xiaomi 13
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/fuxi/fuxi:16/BP2A.250605.031.A3/OS3.0.2.0.WMCCNXM:user/release-keys
