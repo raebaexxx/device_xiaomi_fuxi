df5ac570516dba54d53d7792d97b416d25650e87
raebaexxx <vadimfeda@yandex.ru>
2026-08-20 08:33:31 +0300

    fuxi: Switch to custom_fuxi product and PixelOS config
    Use vendor/custom/config/common_full_phone.mk instead of the LineageOS
    one so that PixelOS versioning (PixelOS_<device>-17.0-<date>.zip) and
    custom packages are picked up, matching other PixelOS devices.

--- custom_fuxi.mk
+++ custom_fuxi.mk
@@ -14,13 +14,11 @@ $(call inherit-product, device/xiaomi/fuxi/device.mk)
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
-# Inherit LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-WITH_GMS := true
+# Inherit PixelOS configurations
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := fuxi
-PRODUCT_NAME := lineage_fuxi
+PRODUCT_NAME := custom_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133C
 PRODUCT_MANUFACTURER := Xiaomi
